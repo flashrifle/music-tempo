13080cbecd9e7856c922da20d3774e5ea9cd4456
flashrifle <flashrifle@gmail.com>
2025-03-12 17:35:57 +0900

    FEAT: Header 영역

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+'use client';
+import styled from 'styled-components';
+
+const HeaderWrap = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #007bff;
+`;
+
+export default function Header() {
+  return <HeaderWrap>Header</HeaderWrap>;
+}
